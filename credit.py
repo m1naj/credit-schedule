cfb9b5ffbcb9171eaf5545d5504284df49bf07d7
m1naj <novokorpusov.mark@mail.ru>
2023-12-20 09:08:01 +0300

    v2

--- credit.py
+++ credit.py
@@ -1,55 +1,40 @@
 from datetime import datetime, timedelta, date
 from pprint import pprint
+from tabulate import tabulate
 
-# Автоматическая дата сегодня
-today = date.today()
-timetoday = today.strftime('%Y-%m-%d')
+def calculate_loan(amount, percent, period):
+    if amount <= 0:
+        raise ValueError("Amount must be a positive number")
 
-amount = input("Введите сумму кредита: ")
-while not amount.isdigit():
-    print("Некорректный ввод! Введите число.")
-    amount = input("Введите сумму кредита: ")
-amount = float(amount)
+    today = date.today()
+    timetoday = today.strftime('%Y-%m-%d')
 
-percent = input("Введите процентную ставку: ")
-while not percent.isdigit():
-    print("Некорректный ввод! Введите число.")
-    percent = input("Введите процентную ставку: ")
-percent = float(percent)
+    def calculate_next_month(date):
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        next_month = date_obj + timedelta(days=31)
+        return next_month.strftime("%Y-%m-%d")
 
-period = input("Введите период кредитования (в месяцах): ")
-while not period.isdigit():
-    print("Некорректный ввод! Введите число.")
-    period = input("Введите период кредитования (в месяцах): ")
-period = int(period)
+    def calculate_monthly_payment(amount, percent, period, i):
+        monthly_payment = (amount / period) + ((amount - (amount / period) * (i - 1)) / 100) * (percent / 12)
+        return monthly_payment
 
-#дата вручную
-# timetoday = str(input("Введите дату начала (ГГГГ-ММ-ДД): "))
+    payment_schedule = []
+    total_payment = 0
 
+    for i in range(1, period+1):
+        monthly_payment = calculate_monthly_payment(amount, percent, period, i)
+        payment_schedule.append([calculate_next_month(timetoday), round(monthly_payment, 2)])
+        total_payment += monthly_payment
+        timetoday = calculate_next_month(timetoday)
 
-def calculate_next_month(date):
-    date_obj = datetime.strptime(date, "%Y-%m-%d")
-    next_month = date_obj + timedelta(days=31)
+    overpayment = total_payment - amount
+    return payment_schedule, round(overpayment, 2)
 
-    return next_month.strftime("%Y-%m-%d")
+amount = 100000
+percent = 12
+period = 12
 
-def calculate_monthly_payment(amount, percent, period, i):
-    monthly_payment = (amount / period) + ((amount - (amount / period) * (i - 1)) / 100) * (percent / 12)
-    
-    return monthly_payment
-
-payment_schedule = {}
-total_payment = 0
-
-for i in range(1, period+1):
-    monthly_payment = calculate_monthly_payment(amount, percent, period, i)
-    payment_schedule[calculate_next_month(timetoday)] = monthly_payment
-    total_payment += monthly_payment
-    timetoday = calculate_next_month(timetoday)
-
-overpayment = total_payment - amount
-K = 1
-res = {key : round(payment_schedule[key], K) for key in payment_schedule}
-print("\nГрафик платы по кредиту: \n")
-pprint(str(res), width = 30)
-print("\nПереплата по кредиту составит:", round(overpayment))
+payment_schedule, overpayment = calculate_loan(amount, percent, period)
+print("Таблица выплат \n")
+print(tabulate(payment_schedule, headers=["Дата", "Месячный платеж"], tablefmt="grid"))
+print("Переплата по кредиту: ", overpayment)
--- test_credit.py
+++ test_credit.py
@@ -1,25 +1,36 @@
-from credit import amount, period, percent
 import pytest
 
-# Сумма кредита
-def test_amount1():
-    assert amount > 0
+from credit import calculate_loan
 
-def test_amount2():
-    assert (amount < 0) == False
+@pytest.mark.parametrize("amount, percent, period, expected_overpayment", [
+    (10000, 5, 12, 270.83),
+    (5000, 3, 6, 43.75),
+    (20000, 8, 24, 1666.67)
+])
+def test_calculate_loan(amount, percent, period, expected_overpayment):
+    payment_schedule, overpayment = calculate_loan(amount, percent, period)
 
-# Процентная ставка
-def test_percent1():
-    assert percent > 0
+    assert len(payment_schedule) == period
+    assert isinstance(overpayment, float)
+    assert overpayment == pytest.approx(expected_overpayment, abs=0.01)
 
-def test_percent2():
-    assert (percent < 0) == False
+def test_calculate_loan_zero_amount():
+    amount = 0
+    percent = 5
+    period = 12
 
-# Период кредиторования
-def test_period1():
-    assert period > 0
+    with pytest.raises(ValueError) as e:
+        calculate_loan(amount, percent, period)
+    
+    assert str(e.value) == "Amount must be a positive number"
+    
 
-def test_period2():
-    assert (period < 0) == False
+def test_calculate_loan_negative_amount():
+    amount = -10000
+    percent = 5
+    period = 12
 
-pytest.main()
+    with pytest.raises(ValueError) as e:
+        calculate_loan(amount, percent, period)
+    
+    assert str(e.value) == "Amount must be a positive number"
